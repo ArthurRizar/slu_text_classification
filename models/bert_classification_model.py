23371139d246b22c04b7d22650f9483d38e141b9
vipufo123 <279424390@qq.com>
2019-04-29 19:02:17 +0800

    update

---
+++ models/bert_classification_model.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+###################################################
+# File Name: bert_fine_tuning.py
+# Author: Meng Zhao
+# mail: @
+# Created Time: 2019年04月29日 星期一 15时32分30秒
+#=============================================================
+
+class BertClassifyModel(object):
+    
