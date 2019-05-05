00e427e6591dd9a84202bfaf071ab07b726316c0
vipufo123 <279424390@qq.com>
2019-05-05 10:38:31 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,8 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#coding:utf-8
+###################################################
+# File Name: __init__.py
+# Author: Meng Zhao
+# mail: @
+# Created Time: 2019年04月29日 星期一 17时28分57秒
+#=============================================================
 
